dc15983e334bff53051faea36a719a536105779a
aleem-ahmed <opall.io2020@gmail.com>
2021-10-26 10:22:54 -0400

    Imprvoed

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,7 @@
 		<!-- UI -->
 		<NavBar />
 
-		<div v-if="!$store.state.isHomePage" class="d-none d-lg-block" style="height: 131px;" />
+		<div v-if="!$store.state.isHomePage" class="d-none d-lg-block" style="height: 126px;" />
 		<div v-if="!$store.state.isHomePage" class="d-block d-lg-none" style="height: 85.11px;" />
 
 		<!-- RouterView -->
